ff1406463fe3e12e9b8cf2f36abb3cc9e11ebfe8
Pavels Vetlugins <declar@inbox.lv>
2018-09-25 11:15:12 +0300

    First commit

---
+++ iOS internship/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS internship
+//
+//  Created by pavels.vetlugins on 25/09/2018.
+//  Copyright © 2018 pavels.vetlugins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
